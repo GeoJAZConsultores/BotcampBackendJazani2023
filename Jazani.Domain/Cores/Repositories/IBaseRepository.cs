1f48c6cf8abd9ff5fc7e163077cfef38f3554865
AbelPalominoRojas <abel.palominorojas@gmail.com>
2023-10-07 10:50:31 -0500

    Start IBaseRepository

---
+++ Jazani.Domain/Cores/Repositories/IBaseRepository.cs
@@ -0,0 +1,13 @@
+﻿using Jazani.Domain.Cores.Models;
+
+namespace Jazani.Domain.Cores.Repositories
+{
+    public interface IBaseRepository<T, ID> where T : CoreModel<ID>
+	{
+        Task<IReadOnlyList<T>> FindAllAsync();
+        Task<T?> FindByIdAsync(ID id);
+        Task<T> SaveAsync(T entity);
+        Task<T?> DisabledByIdAsync(ID id);
+    }
+}
+
---
+++ Jazani.Infrastructure/Cores/Persistences/BaseRepository.cs
@@ -0,0 +1,51 @@
+﻿using Jazani.Domain.Cores.Models;
+using Jazani.Domain.Cores.Repositories;
+using Jazani.Infrastructure.Cores.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace Jazani.Infrastructure.Cores.Persistences
+{
+    public abstract class BaseRepository<T, ID> : IBaseRepository<T, ID> where T : CoreModel<ID>
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private DbSet<T> _dbSet;
+        protected BaseRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+            _dbSet = dbContext.Set<T>();
+        }
+
+        public virtual async Task<T?> DisabledByIdAsync(ID id)
+        {
+            T? entity = await _dbSet.FindAsync(id);
+
+            if (entity is null) throw new Exception("No se encontro informacion para el id: " + id);
+
+            entity.State = false;
+            _dbSet.Update(entity);
+
+            await _dbContext.SaveChangesAsync();
+
+
+            return entity;
+        }
+
+        public async Task<IReadOnlyList<T>> FindAllAsync()
+        {
+            return await _dbSet
+                .Where(x => x.State == true)
+                .ToListAsync();
+        }
+
+        public Task<T?> FindByIdAsync(ID id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> SaveAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
